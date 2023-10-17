fa86a606f5aa839edd42929f42c627f8204ff04c
leo <s950329@hotmail.com>
2023-10-18 00:38:19 +0800

    initial shell script

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $patient = \App\Models\Patient::first();
-
-    $patient->translations()->updateOrCreate([
-        'locale' => 'en',
-        'column' => 'name',
-    ], [
-        'value' => 'Yellow',
-    ]);
+    return 'Hello, world!';
 });
